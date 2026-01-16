00282de62f37f4d8949e60667e19d037de34f610
jihad Orabi <ji.0rabiii@gmail.com>
2026-01-16 20:37:39 +0200

    responsive

--- app/component/deletePop.js
+++ app/component/deletePop.js
@@ -19,8 +19,8 @@ export default function DeleteRate({movie}) {
   return (
     <div className="delete p-4 w-1/2 h-50 m-auto rounded-lg j_bg">
         <p className="h-35">
-  Are you sure you want to delete your rating for
-  <span className="font-bold">{movie?.name || "all movies"}</span>?
+  Are you sure you want to delete your rating for 
+  <span className="font-bold">"{movie?.name || "all movies"}"</span>?
 </p><div className="flex justify-end gap-2">
             <button className="bg-gray-500 rounded-sm text-xs px-2 py-1 cursor-pointer" onClick={closeDeleteModal}>Cancel</button>
             {movie?.name?<button className="bg-blue-400 rounded-sm text-sm px-2 py-1 cursor-pointer" onClick={()=>handleClick(movie.id)}>Delete</button>
--- app/page.js
+++ app/page.js
@@ -5,53 +5,113 @@ import DeleteRate from "./component/deletePop";
 import Movie from "./component/movie";
 import { useMovieStore } from "./store/useMovieStore";
 import useShow from "./store/useShowStore";
+import { FaPlus } from "react-icons/fa";
+
 export default function Home() {
-  const {movies,filterMovies,resetFilter} = useMovieStore()
-  const { isFormOpen,isDeleteOpen, selectedMovie,openFormModal,closeFormModal,openDeleteModal, closeDeleteModal }  = useShow()
+  const { movies, filterMovies, resetFilter } = useMovieStore();
+  const {
+    isFormOpen,
+    isDeleteOpen,
+    selectedMovie,
+    openFormModal,
+    closeFormModal,
+    closeDeleteModal,
+    openDeleteModal
+  } = useShow();
+
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-  const handleScroll = () => {
-    setIsScrolled(window.scrollY > 170);
-  };
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 110);
+    };
 
-  window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const averageRating =
-  movies.length === 0 ? 0 :(movies.reduce((sum, movie) => sum + movie.rating, 0) / movies.length).toFixed(1);
+    movies.length === 0
+      ? 0
+      : (
+          movies.reduce((sum, movie) => sum + movie.rating, 0) / movies.length
+        ).toFixed(1);
+
   return (
-    <div className="font-sans j_bg min-h-screen">
-      <div className="flex justify-start md:justify-between  md:flex-row flex-col gap-2 w-full md:items-center p-8 sm:p-10">
-        <div  className="flex justify-start md:justify-between gap-2 text-xs md:text-sm"><p>Total Movies: {movies.length}</p><p>/</p><p>Average Rating: {averageRating}</p></div>
-        <div className="flex justify-start md:justify-between gap-2">
-          <button className="bg-blue-400 rounded-sm text-xs px-2 py-1 cursor-pointer" onClick={openDeleteModal}>Remove Ratings</button>
-          <button className="bg-blue-400 rounded-sm text-xs px-2 py-1 cursor-pointer" onClick={openFormModal}>Add Movie</button>
+    <div className="font-sans j_bg min-h-screen relative">
+      <div className="flex justify-between gap-2 w-full md:items-center p-8 sm:p-10">
+        <div className="flex text-xs md:text-sm gap-1">
+          <p>Total Movies: {movies.length}</p>
+          <p>/</p>
+          <p>Average Rating: {averageRating}</p>
         </div>
-       
-      </div> <div
-  className={`flex justify-center w-full gap-2 sticky top-0 z-50 p-6 transition-all duration-300
-    ${isScrolled ? "bg-white/10 backdrop-blur-md shadow-md" : "bg-transparent"}
-  `}>
-           <p className="bg-purple-500 text-white text-xs md:text-sm rounded-lg px-4 cursor-pointer" onClick={()=>resetFilter()}>All</p>
-          {["Drama", "Crime", "Action", "Comedy"].map((genre) => (
-            <p className="bg-purple-500 text-white text-xs md:text-sm rounded-lg px-4 cursor-pointer" onClick={()=>filterMovies(genre)}>{genre}</p>
-          ))}
+
+        <div className="flex gap-1">
+          <button className="bg-blue-400 rounded-sm text-xs p-1" onClick={openDeleteModal}>
+            Remove Ratings
+          </button>
+          {!isMobile && (
+            <button className="bg-blue-400 rounded-sm text-xs p-1" onClick={openFormModal}>
+              Add Movie
+            </button>
+          )}
+        </div>
       </div>
-      <div className="flex w-full flex-wrap p-8 gap-4 sm:p-10">
-        {movies.map((movie,index)=>(
-        <Movie key={index} movie={movie}/>
-      ))}</div>
+
+      <div
+        className={`flex justify-center gap-2 sticky top-0 z-10 transition-all duration-300
+        ${isScrolled ? "bg-white/10 backdrop-blur-md shadow-md p-6" : "p-0"}`}
+      >
+        <p className="px-2 rounded-sm bg-purple-500 text-white" onClick={resetFilter}>All</p>
+        {["Drama", "Crime", "Action", "Comedy"].map((genre) => (
+          <p
+            key={genre}
+            className="px-2 rounded-sm bg-purple-500 text-white"
+            onClick={() => filterMovies(genre)}
+          >
+            {genre}
+          </p>
+        ))}
+      </div>
+      <div className="flex flex-wrap p-8 gap-4 sm:p-10">
+        {movies.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+
       {(isDeleteOpen || isFormOpen) && (
-          <>
-            <div className="fixed top-0 overflow- bg-white/5 backdrop-blur-md  bottom-0 right-0 h-full w-full" onClick={()=> {closeDeleteModal(),closeFormModal()}} />
-            <div className="fixed top-1/2 right-1/2 w-full translate-x-1/2 -translate-y-1/2">
-              {isDeleteOpen &&<DeleteRate movie={selectedMovie} />}
-              {isFormOpen &&<AddMovie movie={selectedMovie} />}
-            </div>
-          </>
-        )}
+        <>
+          <div
+            className="fixed inset-0 bg-white/5 backdrop-blur-md z-20"
+            onClick={() => {
+              closeDeleteModal();
+              closeFormModal();
+            }}
+          />
+          <div className="fixed top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 z-20">
+            {isDeleteOpen && <DeleteRate movie={selectedMovie} />}
+            {isFormOpen && <AddMovie movie={selectedMovie} />}
+          </div>
+        </>
+      )}
+
+      {isMobile && (
+        <FaPlus
+          className="bg-blue-400 fixed bottom-5 right-5 rounded-full p-3 text-4xl cursor-pointer"
+          onClick={openFormModal}
+        />
+      )}
     </div>
   );
 }
