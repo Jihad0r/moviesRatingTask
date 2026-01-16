03d85cf79d08daeb5d662cbe36fd0b107f6db67b
jihad Orabi <ji.0rabiii@gmail.com>
2026-01-16 16:46:54 +0200

    update

--- app/component/deletePop.js
+++ app/component/deletePop.js
@@ -19,7 +19,7 @@ export default function DeleteRate({movie}) {
   return (
     <div className="delete p-4 w-1/3 h-50 m-auto rounded-lg j_bg">
         <p className="h-35">
-  Are you sure you want to delete your rating for{" "}
+  Are you sure you want to delete your rating for
   <span className="font-bold">{movie?.name || "all movies"}</span>?
 </p><div className="flex justify-end gap-2">
             <button className="bg-gray-500 rounded-sm text-xs px-2 py-1 cursor-pointer" onClick={closeDeleteModal}>Cancel</button>
